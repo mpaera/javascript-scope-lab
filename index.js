6d806b8ef65e31d86ebc27bc0eb7e26b7d812959
teresia <tereziah.mpaera@student>
2026-04-12 22:10:43 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+// Global scope variables
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function-scoped variable
+function addBurger() {
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
+}
+
+// Block-scoped variable
+if (true) {
+    const anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+}
+
+// Change featured drink
+function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+}
+
+// Call the functions
+addBurger();
+changeFeaturedDrink();
+
+// Print output
+console.log(burgers);
+console.log(featuredDrink);
